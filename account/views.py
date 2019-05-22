aba2a4888647a1eeaf0da1ebbd416e4f738f8f4a
Jay28497 <jaykanjariya28@gmail.com>
2019-05-22 12:30:49 +0530

    create login and logout pages

--- account/views.py
+++ account/views.py
@@ -22,8 +22,18 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'account/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'], password = request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'account/login.html', {'error':'Username or password is incorrect!'})
+    else:
+        return render(request, 'account/login.html')
 
 
 def logout(request):
-    return render(request, 'account/logout.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
