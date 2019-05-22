8d77bc93b74f390615bed1e99817ac8f45acaeec
Jay28497 <jaykanjariya28@gmail.com>
2019-05-23 00:28:00 +0530

    create and uploding products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image'] and request.FILES['icon']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+        else:
+            return render(request, 'products/create.html', {'error': 'please fill all the fields'})
+    else:
+        return render(request, 'products/create.html')
