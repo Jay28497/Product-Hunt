63b3984b3649289b419ae8810940528df625d81c
Jay28497 <jaykanjariya28@gmail.com>
2019-05-24 22:35:18 +0530

    create detail of product page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from .models import Product
 
 
 def home(request):
