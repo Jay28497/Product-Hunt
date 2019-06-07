362644a271afcc1e83df1b83205cb4e9a15421c4
Jay28497 <jaykanjariya28@gmail.com>
2019-06-07 21:08:30 +0530

    UI modify

--- account/views.py
+++ account/views.py
@@ -6,17 +6,19 @@ from django.contrib import auth
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username = request.POST['username'])
-                return render(request, 'account/signup.html', {'error':'Username is already taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
-                auth.login(request, user)
-                return redirect('home')
-        else:
-            return render(request, 'account/signup.html', {'error':'Password doesn\'t matched'})
-
+            if request.POST['username'] != "" and request.POST['password1'] != "" and request.POST['password2'] != "":
+                if request.POST['password1'] == request.POST['password2']:
+                    try:
+                        user = User.objects.get(username = request.POST['username'])
+                        return render(request, 'account/signup.html', {'error':'Username is already taken'})
+                    except User.DoesNotExist:
+                        user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                        auth.login(request, user)
+                        return redirect('home')
+                else:
+                    return render(request, 'account/signup.html', {'error':'Password doesn\'t matched'})
+            else:
+                return render(request, 'account/signup.html', {'error':'Please Fill the all fields.'})
     else:
         return render(request, 'account/signup.html')
 
